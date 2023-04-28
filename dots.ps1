eab1cdd79f474f8009fbe921eb00eb1ec7800680
Robert J Duggan <robert.duggan@live.com>
2023-04-27 22:19:11 -0400

    added script

---
+++ dots.ps1
@@ -0,0 +1,17 @@
+#create a file to source the .vimrc
+New-Item -ItemType file $env:USERPROFILE\.vimrc
+Add-Content $env:USERPROFILE\.vimrc 'source ~/.dots/.vimrc'
+
+#get PowerColorLS and Dependancy
+Install-Module -Name Terminal-Icons -Repository PSGallery
+Import-Module -Name Terminal-Icons
+Install-Module -Name PowerColorLS -Repository PSGallery
+Import-Module PowerColorLS
+
+#get oh-my-posh
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+
+#symlink Powershell profile
+$sourcePath = "$HOME\.dots\Microsoft.PowerShell_profile.ps1"
+$targetPath = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+New-Item -ItemType SymbolicLink -Path $targetPath -Target $sourcePath
