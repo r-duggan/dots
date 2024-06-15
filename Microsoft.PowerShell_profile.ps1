8628ba7d371a23b511455d3af46743579e6c2c39
Robert Duggan <r-duggan@uwbadesign.com>
2024-06-15 16:01:17 -0400

    added project folder generation

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,12 +3,47 @@ Import-Module Terminal-Icons
 Import-Module PowerColorLS
 Import-Module PSReadLine
 
-function upender { 
-    Set-Location -Path "C:\Projects\J49049 (Nikola Hydrogen Tanks Upender)\Programs\Upender"
+function Add-Folders {
+    $currentDir = Get-Location
+
+    if (Test-Path -Path '.\Comm') {
+    } else {
+        New-Item -Path '.\Comm' -Type Directory
+    }
+
+    if (Test-Path -Path '.\Docu') {
+    } else {
+        New-Item -Path '.\Docu' -Type Directory
+    }
+
+    if (Test-Path -Path '.\Dwgs') {
+    } else {
+        New-Item -Path '.\Dwgs' -Type Directory
+    }
+
+    if (Test-Path -Path '.\Expired'){
+    } else {
+        New-Item -Path '.\Expired' -Type Directory
+    }
+
+    if (Test-Path -Path '.\Pics'){
+    } else {
+        New-Item -Path '.\Pics' -Type Directory
+    }
+
+    if (Test-Path -Path '.\Programs'){
+    } else {
+        New-Item -Path '.\Programs' -Type Directory
+    }
+
+    $childDir = Split-Path -Path $currentDir -Leaf
+
+    New-Item ".\$($childDir).txt" -Type File
 }
 
 #Alias
 Set-Alias -Name ls -Value PowerColorLS -Option AllScope
+Set-Alias -Name createBase -Value Add-Folders -Option AllScope
 
 # set environments
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\rduggan\.config\komorebi'
